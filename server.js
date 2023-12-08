b5b48e244f048889687d7d8103e4fcaf444764db
Daniel Jackson <djackson33601@gmail.com>
2023-12-08 07:26:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.sqlite');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
